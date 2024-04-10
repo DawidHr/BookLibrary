561fec2ed49e5b3654625d0d2e2b479702f5f2f9
Cool <DawidH>
2024-04-10 15:01:11 +0200

    Add Author

--- src/main/java/com/dawidhr/BookLibrary/config/Boot.java
+++ src/main/java/com/dawidhr/BookLibrary/config/Boot.java
@@ -1,2 +1,44 @@
-package com.dawidhr.BookLibrary.config;public class Boot {
+package com.dawidhr.BookLibrary.config;
+
+import com.dawidhr.BookLibrary.model.Author;
+import com.dawidhr.BookLibrary.repository.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Boot implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+
+    public Boot(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        prepareAndInsertAuthors();
+    }
+
+    private void prepareAndInsertAuthors() {
+        List<Author> authors = prepareListOfAuthors();
+        insertAuthorsToDatabase(authors);
+    }
+
+    private List<Author> prepareListOfAuthors() {
+        List<Author> authors = new ArrayList<>();
+        authors.add(new Author("Andrzej", "Ziemiański"));
+        authors.add(new Author("Williams Chima", "Cinda"));
+        authors.add(new Author("Trudi", "Canavan"));
+        return authors;
+    }
+
+    private void insertAuthorsToDatabase(List<Author> authors) {
+        for (Author author : authors) {
+            authorRepository.save(author);
+        }
+    }
 }
--- src/main/java/com/dawidhr/BookLibrary/model/Author.java
+++ src/main/java/com/dawidhr/BookLibrary/model/Author.java
@@ -1,2 +1,47 @@
-package com.dawidhr.BookLibrary.model;public class Author {
+package com.dawidhr.BookLibrary.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private Long authorId;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
