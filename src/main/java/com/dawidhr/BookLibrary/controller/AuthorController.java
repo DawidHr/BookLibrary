b3b6fe8e139c57c38924521671f7f1f15b0f3f22
DawidHr <dawiid21@gmail.com>
2024-04-24 12:00:04 +0200

    Add controller and view for author

--- src/main/java/com/dawidhr/BookLibrary/controller/AuthorController.java
+++ src/main/java/com/dawidhr/BookLibrary/controller/AuthorController.java
@@ -1,9 +1,15 @@
 package com.dawidhr.BookLibrary.controller;
 
 import com.dawidhr.BookLibrary.dao.AuthorDAO;
+import com.dawidhr.BookLibrary.model.Author;
+import com.dawidhr.BookLibrary.model.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class AuthorController {
@@ -19,4 +25,17 @@ public class AuthorController {
         model.addAttribute("authors", authorDAO.getAllAuthors());
         return "author/list.html";
     }
+
+
+    @GetMapping("/author/{id}")
+    public String getSelectedAuthor(@PathVariable Long id, Model model) {
+        Optional<Author> author = authorDAO.getAuthorById(id);
+        if (author.isPresent()) {
+            model.addAttribute("author",author.get());
+            return "author/selectedAuthor.html";
+        } else {
+            throw new RuntimeException("Author not found");
+        }
+    }
+
 }
