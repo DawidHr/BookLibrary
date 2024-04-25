a76a8a704753220d8754652b722a71a41fb5e936
DawidHr <dawiid21@gmail.com>
2024-04-25 14:18:34 +0200

    Add validation to Book class

--- src/main/java/com/dawidhr/BookLibrary/model/Book.java
+++ src/main/java/com/dawidhr/BookLibrary/model/Book.java
@@ -1,10 +1,13 @@
 package com.dawidhr.BookLibrary.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -20,6 +23,8 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookId;
     @Column(name = "title")
+    @NotBlank
+    @Length(max=200)
     private String title;
     @ManyToMany()
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -28,9 +33,11 @@ public class Book {
     private String description;
     @Enumerated(EnumType.STRING)
     @Column(name = "category")
+    @NotNull
     private BookCategory category;
     @Enumerated(EnumType.STRING)
     @Column(name = "book_status")
+    @NotNull
     private BookStatus bookStatus;
     @Column(name = "image_url")
     private String imageUrl;
