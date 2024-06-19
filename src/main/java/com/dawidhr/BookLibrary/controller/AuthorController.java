4e8621b0c488a2456db6883a728c1ab2ab79e904
DawidHr <dawiid21@gmail.com>
2024-06-19 12:39:03 +0200

    Add edit view to author

--- src/main/java/com/dawidhr/BookLibrary/controller/AuthorController.java
+++ src/main/java/com/dawidhr/BookLibrary/controller/AuthorController.java
@@ -1,12 +1,15 @@
 package com.dawidhr.BookLibrary.controller;
 
 import com.dawidhr.BookLibrary.dao.AuthorDAO;
+import com.dawidhr.BookLibrary.dao.BookDAO;
 import com.dawidhr.BookLibrary.model.Author;
+import com.dawidhr.BookLibrary.model.Book;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +22,12 @@ import java.util.Optional;
 public class AuthorController {
 
     private final AuthorDAO authorDAO;
+    private final BookDAO bookDAO;
     private static final Integer AUTHOR_PER_PAGE = 2;
 
-    public AuthorController(AuthorDAO authorDAO) {
+    public AuthorController(AuthorDAO authorDAO, BookDAO bookDAO) {
         this.authorDAO = authorDAO;
+        this.bookDAO = bookDAO;
     }
 
     @RequestMapping("/authors")
@@ -42,12 +47,12 @@ public class AuthorController {
 
     @GetMapping("/author/{id}")
     public String getSelectedAuthor(@PathVariable Long id, Model model) {
-        Optional<Author> author = authorDAO.getAuthorById(id);
+        Optional<Author> author = authorDAO.findById(id);
         if (author.isPresent()) {
             model.addAttribute("author",author.get());
             return "author/selectedAuthor.html";
         } else {
-            throw new RuntimeException("Author not found");
+            return "redirect:/error.html";
         }
     }
 
@@ -66,11 +71,46 @@ public class AuthorController {
         return "author/list";
     }
 
+    @GetMapping("/author/{id}/edit")
+    public String editSelectedAuthor(@PathVariable Long id, Model model,HttpServletRequest request) {
+        Optional<Author> author = authorDAO.findById(id);
+        if (author.isPresent()) {
+            model.addAttribute("author",author.get());
+            String bookTitle = request.getParameter("bookTitle");
+            if (StringUtils.hasText(bookTitle)) {
+                model.addAttribute("books",bookDAO.findBook(bookTitle));
+            } else {
+                model.addAttribute("books", bookDAO.getAllAvailableBooks(PageRequest.of(0, 5)));
+            }
+
+            return "author/editAuthor.html";
+        } else {
+            return "redirect:/error.html";
+        }
+    }
+
     @PostMapping("/author/findAuthor")
     public String findAuthor(Model model, HttpServletRequest request) {
         String searchAuthor = request.getParameter("searchAuthor");
         model.addAttribute("authors", authorDAO.findAuthor(searchAuthor));
         return "author/list.html";
+    }
+
+    @GetMapping("/author/{authorId}/book/{bookId}/add")
+    public String assingBookToAuthor(@PathVariable("authorId") long authorId, @PathVariable("bookId") long bookId) {
+        Optional<Author> author = authorDAO.findById(authorId);
+        Optional<Book> book = bookDAO.findById(bookId);
+        if (isBookAndAuthorExist(book, author)) {
+            Author selectedAuthor = author.get();
+            selectedAuthor.addBook(book.get());
+            authorDAO.insertAuthor(selectedAuthor);
+            return "redirect:/author/{authorId}";
+        }
+        return "redirect:/error.html";
+    }
+
+    private boolean isBookAndAuthorExist(Optional<Book> book, Optional<Author> author) {
+        return book.isPresent() && author.isPresent();
     }
 
     private void preparePagination(List<Long> pagination) {
