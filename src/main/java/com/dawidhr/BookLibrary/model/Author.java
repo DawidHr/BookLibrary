4cb8f8a96b77fb449515ab6b0e718ab400e009f7
DawidHr <dawiid21@gmail.com>
2024-06-20 12:55:47 +0200

    add simple validation to form add Author

--- src/main/java/com/dawidhr/BookLibrary/model/Author.java
+++ src/main/java/com/dawidhr/BookLibrary/model/Author.java
@@ -1,6 +1,7 @@
 package com.dawidhr.BookLibrary.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,8 +25,10 @@ public class Author {
     @Column(name = "author_id")
     private Long authorId;
     @Column(name = "first_name")
+    @NotBlank(message = "First Name is required")
     private String firstName;
     @Column(name = "last_name")
+    @NotBlank(message = "Last Name is required")
     private String lastName;
     @ManyToMany(mappedBy = "authors", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private Set<BookInfo> books = new HashSet<>();
