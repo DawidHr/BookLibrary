011f5d54cecf9d09464fdee1163578a2676776a4
Cool <dawiid21@gmail.com>
2024-07-16 21:50:35 +0200

    Person add selected view

--- src/main/java/com/dawidhr/BookLibrary/controller/PersonController.java
+++ src/main/java/com/dawidhr/BookLibrary/controller/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -65,5 +66,25 @@ public class PersonController {
         }
         personDAO.insert(person);
         return "person/list";
+    }
+
+    @GetMapping("/person/{id}/view")
+    public String show(@PathVariable("id") long id, Model model) {
+        Person person = personDAO.getById(id);
+        if (person != null) {
+            model.addAttribute(person);
+            return "person/selectedPerson.html";
+        }
+        return "redirect:/error.html";
+    }
+
+    @GetMapping("/person/{id}/reserve")
+    public String reserve(@PathVariable("id") long id, Model model) {
+        Person person = personDAO.getById(id);
+        if (person != null) {
+            model.addAttribute(person);
+            return "person/selectedPerson.html";
+        }
+        return "redirect:/error.html";
     }
 }
