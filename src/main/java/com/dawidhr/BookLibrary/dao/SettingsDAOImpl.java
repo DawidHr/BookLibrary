95afd2c9d527cdcb3bced9f1d7528b6e00f4c121
DawidHr <dawiid21@gmail.com>
2024-12-11 15:01:47 +0100

    Add save and update logic to SettingsDAOImpl

--- src/main/java/com/dawidhr/BookLibrary/dao/SettingsDAOImpl.java
+++ src/main/java/com/dawidhr/BookLibrary/dao/SettingsDAOImpl.java
@@ -2,8 +2,11 @@ package com.dawidhr.BookLibrary.dao;
 
 import com.dawidhr.BookLibrary.model.Settings;
 import com.dawidhr.BookLibrary.repository.SettingRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class SettingsDAOImpl implements SettingsDAO {
@@ -18,10 +21,20 @@ public class SettingsDAOImpl implements SettingsDAO {
     @Override
     public void updateSettings(Settings settings) {
         //TODO
+        Optional<Settings> settingsFromDb = settingRepository.findById(settings.getSettingsId());
+        if (settingsFromDb.isPresent()) {
+            Settings dbSetting = settingsFromDb.get();
+            dbSetting.setNotificationRate(settings.getNotificationRate());
+            dbSetting.setPrice(settings.getPrice());
+            dbSetting.setNotificationRate(settings.getNotificationRate());
+            settingRepository.save(dbSetting);
+        }
+        throw new EntityNotFoundException();
     }
 
     @Override
     public void insertSettings(Settings settings) {
         //TODO
+        settingRepository.save(settings);
     }
 }
